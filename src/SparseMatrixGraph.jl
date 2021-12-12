da4d609db2bb23655c18f474d0dc23d80e5d78bc
Alfredo Braunstein <alfredo.braunstein@polito.it>
2021-12-12 10:55:04 +0100

    weight -> property

--- src/SparseMatrixGraph.jl
+++
@@ -1,7 +0,0 @@
-module SparseMatrixGraph
-
-include("basics.jl")
-
-export MatrixDiGraph, edges, inedges, outedges, edgetype, has_edge, has_vertex, inneighbors, outneighbors, inedges, outedges, weight, ne, nv, vertices, is_directed
-
-end # module
--- src/basics.jl
+++ src/basics.jl
@@ -1,7 +1,7 @@
 
 using Graphs, SparseArrays
 
-struct MatrixDiGraph{T,TW} <: AbstractGraph{T}
+struct SparseMatrixDiGraph{T,TW} <: AbstractGraph{T}
 	A::SparseMatrixCSC{Bool, T}
 	X::SparseMatrixCSC{T,T}
 	W::TW
@@ -13,42 +13,42 @@ struct IndexedEdge{T}
 	idx::T
 end
 
-function MatrixDiGraph(A::AbstractMatrix{Bool}, W)
+function SparseMatrixDiGraph(A::AbstractMatrix{Bool}, W)
 	A = sparse(A)
 	X = sparse(SparseMatrixCSC(A.m, A.n, A.colptr, A.rowval, collect(1:nnz(A)))')
-	MatrixDiGraph(A, X, W)
+	SparseMatrixDiGraph(A, X, W)
 end
 
-function MatrixDiGraph(A::AbstractMatrix)
-	MatrixDiGraph(SparseMatrixCSC(A.m, A.n, A.colptr, A.rowval, fill(true, length(A.nzval))), A.nzval)
+function SparseMatrixDiGraph(A::AbstractMatrix)
+	SparseMatrixDiGraph(SparseMatrixCSC(A.m, A.n, A.colptr, A.rowval, fill(true, length(A.nzval))), A.nzval)
 end
 
-edges(g::MatrixDiGraph) = (IndexedEdge{Int}(i,g.A.rowval[k],k) for i=1:size(g.A,2) for k=nzrange(g.A,i))
+edges(g::SparseMatrixDiGraph) = (IndexedEdge{Int}(i,g.A.rowval[k],k) for i=1:size(g.A,2) for k=nzrange(g.A,i))
 
-Base.eltype(g::MatrixDiGraph{T}) where T= T
+Base.eltype(g::SparseMatrixDiGraph{T}) where T= T
 
-edgetype(g::MatrixDiGraph{T}) where T = IndexedEdge{T}
+edgetype(g::SparseMatrixDiGraph{T}) where T = IndexedEdge{T}
 
-has_edge(g::MatrixDiGraph, i, j) = g.A[i,j]
+has_edge(g::SparseMatrixDiGraph, i, j) = g.A[i,j]
 
-has_vertex(g::MatrixDiGraph, i) = 1 ≤ i ≤ size(g.A, 2)
+has_vertex(g::SparseMatrixDiGraph, i) = 1 ≤ i ≤ size(g.A, 2)
 
-inneighbors(g::MatrixDiGraph, i) = @view g.A.rowval[nzrange(g.A,i)]
+inneighbors(g::SparseMatrixDiGraph, i) = @view g.A.rowval[nzrange(g.A,i)]
 
-outneighbors(g::MatrixDiGraph, i) = @view g.A.rowval[g.X.nzval[nzrange(g.X,i)]]
+outneighbors(g::SparseMatrixDiGraph, i) = @view g.A.rowval[g.X.nzval[nzrange(g.X,i)]]
 
-outedges(g::MatrixDiGraph, i) = (IndexedEdge{Int}(i,g.A.rowval[k],k) for k=nzrange(g.A,i))
+outedges(g::SparseMatrixDiGraph, i) = (IndexedEdge{Int}(i,g.A.rowval[k],k) for k=nzrange(g.A,i))
 
-inedges(g::MatrixDiGraph, i) = (IndexedEdge{Int}(g.X.rowval[k],i,k) for k=@view g.X.nzval[nzrange(g.X,i)])
+inedges(g::SparseMatrixDiGraph, i) = (IndexedEdge{Int}(g.X.rowval[k],i,k) for k=@view g.X.nzval[nzrange(g.X,i)])
 
-weight(g::MatrixDiGraph, e::IndexedEdge) = g.W[e.idx]
+property(g::SparseMatrixDiGraph, e::IndexedEdge) = g.W[e.idx]
 
-ne(g::MatrixDiGraph) = nnz(g.A)
+ne(g::SparseMatrixDiGraph) = nnz(g.A)
 
-nv(g::MatrixDiGraph) = size(A,2)
+nv(g::SparseMatrixDiGraph) = size(A,2)
 
-vertices(g::MatrixDiGraph) = 1:size(A,2)
+vertices(g::SparseMatrixDiGraph) = 1:size(A,2)
 
-is_directed(g::MatrixDiGraph) = true
+is_directed(g::SparseMatrixDiGraph) = true
 
-zero(g::MatrixDiGraph) = MatrixDiGraph(zero(A), @view W[1:0])
+zero(g::SparseMatrixDiGraph) = SparseMatrixDiGraph(zero(A), @view W[1:0])

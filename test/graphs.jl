c2abd876e93250d048ed92c1d6a6bc217cabc4bc
Alfredo Braunstein <alfredo.braunstein@polito.it>
2021-12-13 22:31:38 +0100

    simplify tests

--- test/graphs.jl
+++ test/graphs.jl
@@ -3,8 +3,8 @@ using Graphs, SparseArrays, LinearAlgebra
 @testset "graphs" begin
 	A = sparse(I(10))
 	B = spdiagm(1=>trues(9),-1=>trues(9))
-	gA = SparseMatrixDiGraph(A, fill(π,length(nonzeros(A))))
-	gB = SparseMatrixDiGraph(B, fill(π,length(nonzeros(B))))
+	gA = SparseMatrixDiGraph(A)
+	gB = SparseMatrixDiGraph(B)
 	@test is_directed(gA)
 	@test !is_connected(gA)
 	@test is_connected(gB)

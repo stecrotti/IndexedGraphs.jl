c1808f3373694ca9ab466f7c0778040a1b08c54e
stecrotti <ruggentipini@gmail.com>
2022-01-14 13:21:09 +0100

    dijkstra as script with benchmarks

--- src/algorithms/dijkstra.jl
+++ src/algorithms/dijkstra.jl
@@ -1,3 +1,4 @@
+### Homemade version to compare with MatrixNetworks
 using TrackingHeaps
 
 function dijkstra(g::AbstractSparseMatrixGraph{T}, s::Integer, 
@@ -33,10 +34,6 @@ for i in 1:size(W,2); W[i,i] = 0; end
 dropzeros!(W)
 s = rand(1:size(W,2))
 
-# W = spzeros(10, 10)
-# W[2,3] = W[3,4] = W[4,5] = 1
-# s = 2
-
 g = SparseMatrixDiGraph(W)
 g_MN = MatrixNetwork(W)
 
@@ -50,5 +47,112 @@ d_MN, p_MN = MatrixNetworks.dijkstra(g_MN, s)
 @assert p_SMG == p_MN
 
 using BenchmarkTools
+println("SparseMatrixGraphs:")
 @btime dijkstra($g, $s, $w)
+println("MatrixNetworks:")
 @btime MatrixNetworks.dijkstra($g_MN, $s);
+
+
+### Override for Graphs' version, copied from https://github.com/JuliaGraphs/Graphs.jl/blob/master/src/shortestpaths/dijkstra.jl
+using DataStructures, Graphs, SimpleWeightedGraphs
+import Graphs.DijkstraState
+
+function Graphs.dijkstra_shortest_paths(g::AbstractSparseMatrixGraph,
+    srcs::Vector{U},
+    distvec::AbstractVector{T}=ones(Int, ne(g));
+    allpaths=false,
+    trackvertices=false
+    ) where T <: Real where U <: Integer
+
+    nvg = nv(g)
+    dists = fill(typemax(T), nvg)
+    parents = zeros(U, nvg)
+
+    pathcounts = zeros(nvg)
+    preds = fill(Vector{U}(), nvg)
+    H = PriorityQueue{U,T}()
+
+    for src in srcs
+        dists[src] = zero(T)
+        pathcounts[src] = one(Float64)
+        H[src] = zero(T)
+    end
+
+    closest_vertices = Vector{U}()  # Maintains vertices in order of distances from source
+    sizehint!(closest_vertices, nvg)
+
+    while !isempty(H)
+        u = dequeue!(H)
+
+        if trackvertices
+            push!(closest_vertices, u)
+        end
+
+        d = dists[u] # Cannot be typemax if `u` is in the queue
+        for e in outedges(g, u)
+            alt = d + distvec[idx(e)]
+            v = dst(e)
+
+            visited = dists[v] != typemax(T)
+            if !visited
+                dists[v] = alt
+                parents[v] = u
+
+                pathcounts[v] += pathcounts[u]
+                if allpaths
+                    preds[v] = [u;]
+                end
+                H[v] = alt
+            elseif alt < dists[v]
+                dists[v] = alt
+                parents[v] = u
+                #615
+                pathcounts[v] = pathcounts[u]
+                if allpaths
+                    resize!(preds[v], 1)
+                    preds[v][1] = u
+                end
+                H[v] = alt
+            elseif alt == dists[v]
+                pathcounts[v] += pathcounts[u]
+                if allpaths
+                    push!(preds[v], u)
+                end
+            end
+        end
+    end
+
+    if trackvertices
+        for s in vertices(g)
+            visited = dists[s] != typemax(T)
+            if !visited[s]
+                push!(closest_vertices, s)
+            end
+        end
+    end
+
+    for src in srcs
+        pathcounts[src] = one(Float64)
+        parents[src] = 0
+        empty!(preds[src])
+    end
+
+    return DijkstraState{T,U}(parents, dists, preds, pathcounts, closest_vertices)
+end
+
+Graphs.dijkstra_shortest_paths(g::AbstractSparseMatrixGraph, src::Integer, distvec::AbstractVector=ones(Int, ne(g)); allpaths=false, trackvertices=false) =
+Graphs.dijkstra_shortest_paths(g, [src;], distvec; allpaths=allpaths, trackvertices=trackvertices)
+
+g_Graphs = SimpleWeightedDiGraph(W)
+
+ds_SMG = Graphs.dijkstra_shortest_paths(g, s, w)
+ds_Graphs = Graphs.dijkstra_shortest_paths(g_Graphs, s)
+
+@assert ds_SMG.dists == ds_Graphs.dists
+@assert ds_SMG.parents == ds_Graphs.parents
+
+println()
+println("SparseMatrixGraphs:")
+@btime Graphs.dijkstra_shortest_paths($g, $s, $w)
+println("SimpleWeightedGraphs:")
+@btime Graphs.dijkstra_shortest_paths($g_Graphs, $s);

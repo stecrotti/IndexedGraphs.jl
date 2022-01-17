0dc5a4f4b40bd3d5acfdc64a7a7a686791f47691
stecrotti <ruggentipini@gmail.com>
2022-01-17 13:33:25 +0100

    trying to host docs online

--- docs/make.jl
+++ docs/make.jl
@@ -1,2 +1,6 @@
 using Documenter, IndexedGraphs
 makedocs(sitename="IndexedGraphs Documentation", format = Documenter.HTML(prettyurls = false))
+
+deploydocs(
+    repo = "github.com/stecrotti/IndexedGraphs.jl.git",
+)
